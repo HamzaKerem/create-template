4ed1d94b6cb1ff29abcb27a04dbda3d322c8045b
HamzaKerem <hamzamusic34@gmail.com>
2021-06-21 11:55:30 +0300

    Removed --revert

--- ct.sh
+++ ct.sh
@@ -6,7 +6,7 @@ ALT_PROGRAM_NAME="create-template"
 LICENSE="GNU GPLv3"
 VERSION="1.0"
 AUTHOR="Hamza Kerem Mumcu <hamzamumcu@protonmail.com>"
-USAGE="Usage: $PROGRAM_NAME [-g|--generate] [-r|--reset] [-n|--no-edit] [-m|--make-executable]
+USAGE="Usage: $PROGRAM_NAME [-g|--generate] [-n|--no-edit] [-m|--make-executable]
 [-q|--quiet] [-s|--no-messages] [-h|--help] [-v|--version] FILE [TEMPLATE]"
 
 # Configuration Data
